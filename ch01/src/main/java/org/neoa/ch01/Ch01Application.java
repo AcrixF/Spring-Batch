a377c4d5315794fcb38adfb91631e6496839c36d
Cristian Flores <acrixf@gmail.com>
2022-05-21 23:34:44 -0500

    Initial commit Spring Batch

---
+++ ch01/src/main/java/org/neoa/ch01/Ch01Application.java
@@ -0,0 +1,49 @@
+package org.neoa.ch01;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@EnableBatchProcessing
+@SpringBootApplication
+@Log4j2
+public class Ch01Application {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Autowired
+    public Ch01Application(JobBuilderFactory jobBuilderFactory, StepBuilderFactory stepBuilderFactory) {
+        this.jobBuilderFactory = jobBuilderFactory;
+        this.stepBuilderFactory = stepBuilderFactory;
+    }
+
+    @Bean
+    public Step step() {
+        return this.stepBuilderFactory.get("step1")
+                .tasklet((stepContribution, chunkContext) -> {
+                    log.info("Hello World!");
+                    return RepeatStatus.FINISHED;
+                }).build();
+    }
+
+    @Bean
+    public Job job() {
+        return this.jobBuilderFactory.get("job")
+                .start(step())
+                .build();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ch01Application.class, args);
+    }
+
+}
---
+++ ch01/src/test/java/org/neoa/ch01/Ch01ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.neoa.ch01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ch01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
